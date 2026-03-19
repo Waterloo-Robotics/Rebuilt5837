29c58347ae51ad288ecf2179225aee059fbd7531
Waterloo Robotics <ftcteam7247@gmail.com>
2026-03-18 19:28:51 -0500

    ksakdlka

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -48,4 +48,72 @@ public final class Constants {
     public static final double kMaxDriveSpeed_m_s = Units.feetToMeters(5);
     public static final double kMaxSpinSpeed_rev_s = 0.2; // Rps
   }
+
+  public static class Intake {
+    public static final double kDriveRatio = 7.03;
+    /* Intake PID */
+    public static final double kIntakeP = 0.048;
+    public static final double kIntakeI = 0.0;
+    public static final double kIntakeD = 0.0;
+    public static final double kIntakeTolerance = 1;
+    public static final double kMaxIntakeVoltage = 8;
+    /* Rotate PID */
+    public static final double kRotateP = 0.0;
+    public static final double kRotateI = 0.0;
+    public static final double kRotateD = 0.0;
+    /* Rotate Feedforward */
+    public static final int kMotorkS = 0;
+      // kV for in Volts * RPM
+    public static final double kMotorkV = 0.002;
+    public static final double kFeedForwardMaxVoltage = 12;
+    public static final double kClosedLoopMaxVoltage = 0;
+  }
+
+public static class HotDog {
+    public static final double kDriveRatio = 7.03;
+
+    /* HotDog PID */
+    public static final double kHotDogP = 0.048;
+    public static final double kHotDogI = 0.0;
+    public static final double kHotDogD = 0.0;
+    public static final double kHotDogTolerance = 1;
+    public static final double kMaxHotDogVoltage = 8;
+  
+    // kV for in Volts * RPM
+    public static final double kMotorkV = 0.002;
+    public static final double kFeedForwardMaxVoltage = 12;
+    public static final double kClosedLoopMaxVoltage = 0;
+  }
+
+public static class Climber {
+    public static final double kDriveRatio = 7.03;
+
+    /* Climber PID */
+    public static final double kRotateP = 0.0;
+    public static final double kRotateI = 0.0;
+    public static final double kRotateD = 0.0;
+    /* Climber Feedforward */
+    public static final int kMotorkS = 0;
+    // kV for in Volts * RPM
+    public static final double kMotorkV = 0.002;
+    public static final double kFeedForwardMaxVoltage = 12;
+    public static final double kClosedLoopMaxVoltage = 0;
+  }
+
+  public static class Flywheel {
+    public static final double kDriveRatio = 7.03;
+
+    /* Flywheel 1 PID */
+    public static final double kFlywheelMotor1P = 0.0;
+    public static final double kFlywheelMotor1I = 0.0;
+    public static final double kFlywheelMotor1D = 0.0;
+    /* Flywheel 2 PID */
+    public static final double kFlywheelMotor2P = 0.0;
+    public static final double kFlywheelMotor2I = 0.0;
+    public static final double kFlywheelMotor2D = 0.0;
+    // kV for in Volts * RPM
+    public static final double kMotorkV = 0.002;
+    public static final double kFeedForwardMaxVoltage = 12;
+    public static final double kClosedLoopMaxVoltage = 0;
+  }
 }
